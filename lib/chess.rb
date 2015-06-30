9f58ad419cba79c443ec56f74c406f6b4b7cccc7
Josh Cheek <josh.cheek@gmail.com>
2015-06-29 19:12:27 -0600

    Pseudocode into "wouldn't it be nice" code :)

--- lib/chess.rb
+++ lib/chess.rb
@@ -6,19 +6,30 @@ class Chess
 
     def call
       while game.playing?
-        print the board
-        print whose turn it is
-        prompt for which piece to move
-        list possible moves
-        prompt for location to move to
-        empty line
-        print move summary
-        empty line
+        print_board game.board
+        print_turn
 
-      print the board
-      empty line
-      game summary (winner)
+        from = prompt_from_location
+        print_available_moves(from)
+        to = prompt_to_location
+        game.move from, to
+
+        empty_line
+        print_move_summary from, to
+        empty line
+      end
+
+      print_board game.board
+      empty_line
+      print_game_summary
       0
     end
+
+    # game.moves_for(from_location)
+
+    def print_turn(player_colour)
+      @stdout.puts "#{player_colour}'s turn"
+    end
+
   end
 end
